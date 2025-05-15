264703afea31084beb4e03e44e25ef1c8076773b
inibeliaa <beliaseptiramadhaani@gmail.com>
2025-05-15 22:36:57 +0700

    package

--- src/pages/UmrohPackage/[id]/PackageDetail.tsx
+++ src/pages/UmrohPackage/[id]/PackageDetail.tsx
@@ -1,14 +1,25 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import DefaultLayout from '../../../layout/DefaultLayout'
 import hotelIcon from "../../../assets/icons/Component 1.svg"
 import wifiIcon from "../../../assets/icons/ic_baseline-wifi.svg"
+import hotelIcons from "../../../assets/icons/Group.svg"
 import disabilityIcon from '../../../assets/icons/streamline_travel-wayfinder-disability-person-access-wheelchair-accomodation-human-disability-disabled-user.svg'
 import spoonIcon from "../../../assets/icons/ic_round-restaurant.svg"
 import roomServiceIcon from '../../../assets/icons/ic_baseline-meeting-room.svg'
 import locationIcon from "../../../assets/icons/Pin_alt.svg"
 import exampleImage from "../../../assets/images/pexels-sultan-alhuthali-175963006-18274181.png"
 import CardReview from '../../../components/Card/CardReview'
+import planeIcon from '../../../assets/icons/bxs_plane.svg'
 import TravelSchedule from '../../../components/TravelSchedule'
+import examplePlane from '../../../assets/images/image 3.png'
+import bedIcons from '../../../assets/icons/mdi_guest-room.svg'
+import departureLocIcon from '../../../assets/icons/mingcute_location-fill.svg'
+import dateIcon from '../../../assets/icons/clarity_date-solid.svg'
+import durationIcon from '../../../assets/icons/mdi_calendar-time.svg'
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function PackageDetail() {
     const [currentPage, setCurrentPage] = useState(1);
@@ -71,21 +82,207 @@ function PackageDetail() {
     const startIndex = (currentPage - 1) * itemPages
     const currentItems = schedule.slice(startIndex, startIndex + itemPages)
 
+    const images = [
+        exampleImage,
+        exampleImage,
+        exampleImage
+      ];
+    
+      const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+    };
+    
+    const sliderRef = useRef<Slider>(null);
+
+    const next = () => {
+        if (sliderRef.current) {
+            sliderRef.current.slickNext();
+        }
+    };
+
+    const previous = () => {
+        if (sliderRef.current) {
+            sliderRef.current.slickPrev();
+        }
+    };
     return (
         <DefaultLayout>
             <div className='w-full pt-[200px] min-h-screen background-div flex flex-col space-y-[25px] items-center justify-center'>
-                <div className="grid grid-cols-5 gap-5 w-10/12">
-                    <div className="bg-white col-span-2 h-[500px] rounded-[5px]">
+            <div className="grid grid-cols-5 gap-5 w-10/12 mx-auto mt-10">
+            {/* Kartu Kiri */}
+            <div className="bg-white col-span-2 h-[550px] rounded-[5px] shadow-md overflow-hidden">
+            <div className="relative rounded-t-[5px] overflow-hidden">
+                <Slider {...settings} ref={sliderRef}>
+                    {images.map((src, index) => (
+                    <div key={index} className="h-[220px]">
+                        <img
+                        src={src}
+                        alt={`slide-${index}`}
+                        className="w-full h-[220px] object-cover"
+                        />
                     </div>
-                    <div className=" col-span-3 h-[500px] flex-col flex space-y-6 rounded-[5px]">
-                        <div className="bg-white w-full h-10/12 rounded-[5px]">
+                    ))}
+                </Slider>
+                <button
+                className="absolute left-16 top-1/2 transform -translate-y-1/2 z-10 bg-black/50 w-[24px] h-[24px] text-white font-bold items-center justify-center flex p-2 rounded-full"
+                onClick={previous}
+            >
+               &lt;
 
+            </button>
+            <button
+                className="absolute right-16 top-1/2 transform -translate-y-1/2 z-10 bg-black/50 w-[24px] h-[24px] text-white font-bold items-center justify-center flex p-2 rounded-full"
+                onClick={next}
+            >
+               &gt;
+
+            </button>
+
+                {/* Overlay dan teks */}
+                <div className="absolute bottom-2 left-0 w-full h-[50px] bg-black/50 flex items-center px-8">
+                    <p className="text-white text-[15px] font-semibold">
+                    Paket <span className="text-[#10F5EA] ms-4 text-[20px]">Plus</span>
+                    </p>
+                </div>
+            </div>
+
+                        
+                <div className="px-8 py-4 space-y-2">
+                <p className="text-[15px] font-medium">Harga</p>
+                <h2 className="text-[36px] font-bold bg-gradient-to-r from-[#3C97FF] to-[#245B99] bg-clip-text text-transparent">
+                Rp 32.000.000
+                </h2>
+
+                <p className="text-[15px] font-medium">Mitra</p>
+                <div className="flex items-center space-x-4">
+                    <span className="text-[24px] font-medium">Hasanah Hana</span>
+                    <span className='text-yellow-300 text-[18px]'>★ ★ ★ ★ ★</span>
+                </div>
+                <p className="text-[15px] font-medium">Nomor Izin Umroh</p>
+                <p className="text-[20px] font-medium">1243214533</p>
+                <div className="mt-7 w-10/12 h-[14px]">
+                    <div className="w-full bg-gray-200 rounded-full h-full">
+                    <div
+                        className="bg-gradient-to-r from-[#004492] to-[#10F5EA] h-full rounded-full w-8/12"
+                        
+                    ></div>
+                    </div>
+                    <p className="text-[12px] mt-1 font-semibold">Pesanan: 100/150</p>
+                </div>
+                </div>
+            </div>
+
+            {/* Kartu Kanan */}
+            <div className="col-span-3 h-[550px] flex flex-col space-y-6">
+                <div className="bg-white w-full h-10/12 rounded-[5px] py-4 px-[50px] space-y-4 shadow-md">
+                <h2 className="font-semibold text-[24px] ms-3">Fasilitas Umroh</h2>
+                <div className="grid grid-cols-2 gapx-8 py-4 text-sm">
+                    <ul className="list-disc list-inside text-[15px] font-medium space-y-1">
+                    <li>lorem ipsum dolor sit amet</li>
+                    <li>lorem ipsum dolor sit amet</li>
+                    <li>lorem ipsum dolor sit amet</li>
+                    <li>lorem ipsum dolor sit amet</li>
+                    <li>lorem ipsum dolor sit amet</li>
+                    </ul>
+                    <ul className="list-disc list-inside text-[15px] font-medium space-y-1">
+                    <li>lorem ipsum dolor sit amet</li>
+                    <li>lorem ipsum dolor sit amet</li>
+                    <li>lorem ipsum dolor sit amet</li>
+                    <li>lorem ipsum dolor sit amet</li>
+                    </ul>
+                </div>
+
+                <div className="grid grid-cols-2 gapx-8 py-4 pt-4 text-sm border-t">
+                    <div>
+                    <div className="flex space-x-2 h-[16px] items-center">
+                    <img src={bedIcons} alt='Hotels' className='w-[14px] h-[14px]'/>
+                    <p className="font-bold text-[15px]">Pilihan Kamar</p>
+                    </div>
+                    <p className='font-medium text-[13px] ms-6 mt-3'>Satu Kamar 4 Orang <span className='font-semibold'>(QUAD)</span></p>
+                    </div>
+                    <div>
+                    <div className="flex space-x-2 h-[16px] items-center">
+                    <img src={hotelIcons} alt='Hotels' className='w-[14px] h-[14px]'/>
+                    <p className="font-bold text-[15px]">Hotel</p>
+                    </div>
+                    <div className="flex space-x-1 ms-4 mt-3">
+                        <div className="h-[16px] flex items-center">         
+                        <img src={hotelIcon} alt="icon" className='w-[11px] h-[11px]' />
                         </div>
-                        <div className="bg-gradient-to-r flex items-center justify-center from-[#10F5EA] to-[#3C97FF]  w-full h-2/12 rounded-b-[20px] rounded-t-[5px]">
-                            <h1 className='capitalize text-white text-center font-bold text-[30px]'>Pesan sekarang</h1>
+                        <div className='flex-col w-full'>
+                                
+                                                    <h6 className='text-[12px] font-semibold'>
+                                                        Hotel Habib
+                                                        <span className='text-yellow-300 mx-[7px]'>★ ★ ★ ★ ★</span> Quad Room
+                                                    </h6>
+                            
+                                                    <p className='text-[#209FB2] text-[10px] capitalize font-semibold'>200 m ke masjidil haram</p>
+                                                </div>
+                    </div>
+                    </div>
+                    <div>
+                    <div className="flex space-x-2 h-[16px] items-center">
+                    <img src={durationIcon} alt='Hotels' className='w-[14px] h-[14px]'/>
+                    <p className="font-bold text-[15px]">Duration Perjalanan</p>
+                    </div>
+                    <p className='font-medium text-[13px] ms-6 mt-1'>9 Hari</p>
+                    </div>
+                    <div className="flex space-x-1 ms-4">
+                        <div className="h-[16px] flex items-center">         
+                        <img src={hotelIcon} alt="icon" className='w-[11px] h-[11px]' />
                         </div>
+                        <div className='flex-col w-full'>
+                                
+                                                    <h6 className='text-[12px] font-semibold'>
+                                                        Hotel Hanawi
+                                                        <span className='text-yellow-300 mx-[7px]'>★ ★ ★ ★ ★</span> Quad Room
+                                                    </h6>
+                            
+                                                    <p className='text-[#209FB2] text-[10px] capitalize font-semibold'>200 m ke masjidil haram</p>
+                                                </div>
+                    </div>
+                    <div>
+                    <div className="flex space-x-2 h-[16px] items-center">
+                    <img src={dateIcon} alt='Hotels' className='w-[14px] h-[14px]'/>
+                    <p className="font-bold text-[15px]">Tanggal Keberangkatan</p>
+                    </div>
+                    <p className='font-medium text-[13px] ms-6 mt-1'>10 September 2023</p>
+                    </div>
+                    <div>
+                    <div className="h-[16px] flex items-center space-x-2">         
+                        <img src={planeIcon} alt="icon" className='w-[14px] h-[14px]' />
+                    <p className="font-semibold">Maskapai</p>
+                    </div>
+                    <p className='font-medium text-[13px] ms-6 mt-1'>Garuda</p>
+                    
+                    </div>
+                    <div>
+                    <div className="flex space-x-2 h-[16px] items-center">
+                    <img src={departureLocIcon} alt='Hotels' className='w-[14px] h-[14px]'/>
+                    <p className="font-bold text-[15px]">Kota Keberangkatan</p>
+                    </div>
+                    <p className='font-medium text-[13px] ms-6 mt-1'>Jakarta</p>
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <img src={examplePlane} alt="AirAsia" className="w-[34px] h-[34px] " />
+                        <span className='font-semibold'>Air Asia Airlines</span>
                     </div>
                 </div>
+                </div>
+
+                <div className="bg-gradient-to-r from-[#10F5EA] to-[#3C97FF] w-full h-2/12 flex items-center justify-center rounded-b-[20px] rounded-t-[5px] shadow-md">
+                <h1 className="capitalize text-white text-center font-bold text-[30px]">
+                    Pesan Sekarang
+                </h1>
+                </div>
+            </div>
+            </div>
+
 
                 {/* hotel dan Fasilitasnya */}
                 <div className="w-10/12 h-[600px] p-[40px] rounded-[5px] bg-white">
