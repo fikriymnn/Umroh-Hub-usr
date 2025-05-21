0ea3aac9dd79bd6fc9a38c70d244b1f40f3ea0ef
inibeliaa <beliaseptiramadhaani@gmail.com>
2025-05-21 10:47:16 +0700

    payment

---
+++ src/pages/Payment/Payment.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DefaultLayout from '../../layout/DefaultLayout'
+
+function Payment() {
+    return (
+      <DefaultLayout>           
+        <div>
+            
+        </div>
+      </DefaultLayout>
+  )
+}
+
+export default Payment
