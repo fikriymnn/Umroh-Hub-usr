a5be87af6426dcb3cd29f0dfa1c95bb5c6d6b5ea
IrfanSahruli <irfansahruli244@gmail.com>
2025-05-27 16:22:27 +0700

    Profile

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ export default function App() {
               </>
             }
           />
-           <Route
+          <Route
             path="/Profile"
             element={
               <>
